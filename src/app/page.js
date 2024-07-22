b73f6c80c756fb6609b05b189184a409a2ad4a1b
vaibdix <dixitvaib@gmail.com>
2024-07-22 19:50:53 +0530

    init

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Scene from '@/components/Scene';
+import Text from '@/components/Text';
+
+export default function Home() {
+  return (
+    <main className="flex w-full h-screen items-center justify-center bg-[#fe6120]">
+
+      <Text />
+      <Scene />
+    </main>
+  );
+}
---
+++ src/components/Text.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight text-[#000000] ">
+      <p color="text-orange-600">Custom Text</p>
+      <p className="self-end">Paint Eraser</p>
+    </div>
+  );
+}
